d7eabcecdd93cb39d832c48782cd195f631dc55b
Krispy <89738879+KrispyJT@users.noreply.github.com>
2025-06-09 14:52:33 +0000

    Updated UIS to IS

--- pages/Metrics_Glossary.py
+++ pages/Metrics_Glossary.py
@@ -33,7 +33,7 @@ Understanding the **purpose** of each service category helps clarify why certain
 # -------------------
 st.header("📏 Metric Type Definitions")
 
-with st.expander("🔹 Unique Individuals Served (UIS)"):
+with st.expander("🔹Individuals Served (UIS)"):
     st.markdown("""
     Represents the **number of distinct individuals** served by a program.
 
